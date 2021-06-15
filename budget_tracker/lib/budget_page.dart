27c02fb9b3f2eba3581307142e980c40ee1860a0
jamescardona11 <jjcardonao@gmail.com>
2021-06-15 15:33:32 -0500

    list all items

--- budget_tracker/lib/budget_page.dart
+++ budget_tracker/lib/budget_page.dart
@@ -1,16 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class BudgetPage extends StatelessWidget {
+import 'budget_repository.dart';
+import 'failure_model.dart';
+import 'item_model.dart';
+
+class BudgetPage extends StatefulWidget {
   const BudgetPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  _BudgetPageState createState() => _BudgetPageState();
+}
+
+class _BudgetPageState extends State<BudgetPage> {
+  late Future<List<Item>> _futureItems;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureItems = BudgetRepository().getItems();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello New Page'),
+      appBar: AppBar(
+        title: const Text('Budget Tracker'),
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _futureItems = BudgetRepository().getItems();
+          setState(() {});
+        },
+        child: FutureBuilder<List<Item>>(
+          future: _futureItems,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              // Show pie chart and list view of items.
+              final items = snapshot.data!;
+              return ListView.builder(
+                itemCount: items.length + 1,
+                itemBuilder: (BuildContext context, int index) {
+                  // if (index == 0) return SpendingChart(items: items);
+
+                  final item = items[index - 1];
+                  return Container(
+                    margin: const EdgeInsets.all(8.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(
+                        width: 2.0,
+                        color: getCategoryColor(item.category),
+                      ),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.black26,
+                          offset: Offset(0, 2),
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(
+                        '${item.category} • ${DateFormat.yMd().format(item.date)}',
+                      ),
+                      trailing: Text(
+                        '-\$${item.price.toStringAsFixed(2)}',
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (snapshot.hasError) {
+              // Show failure error message.
+              final failure = snapshot.error as Failure;
+              return Center(child: Text(failure.message));
+            }
+            // Show a loading spinner.
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
+  }
+}
+
+Color getCategoryColor(String category) {
+  switch (category) {
+    case 'Entertainment':
+      return Colors.red[400]!;
+    case 'Food':
+      return Colors.green[400]!;
+    case 'Personal':
+      return Colors.blue[400]!;
+    case 'Transportation':
+      return Colors.purple[400]!;
+    default:
+      return Colors.orange[400]!;
   }
 }
