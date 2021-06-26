7562b3592799d12f6626fee50f3da25717f358ec
jamescardona11 <jjcardonao@gmail.com>
2021-06-25 21:20:08 -0500

    Finish the UI

---
+++ wallet_blockchain/lib/home_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:web3dart/web3dart.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Client httpClient;
+  late Web3Client ethClient;
+
+  final address = '0xEc0B19814E98c93eD6F0859fEdE4f92F40bD1431';
+  bool data = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // httpClient = Client();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Vx.gray300,
+      body: ZStack([
+        VxBox()
+            .blue600
+            .size(
+              context.screenWidth,
+              context.percentHeight * 30,
+            )
+            .make(),
+        VStack([
+          (context.percentHeight * 10).heightBox,
+          '\$PKCoin'.text.xl4.white.bold.center.makeCentered().py16(),
+          (context.percentHeight * 5).heightBox,
+          VxBox(
+            child: VStack(
+              [
+                'Balance'.text.gray700.xl2.semiBold.makeCentered(),
+                10.heightBox,
+                data
+                    ? "\$1".text.bold.xl6.makeCentered()
+                    : CircularProgressIndicator().centered()
+              ],
+            ),
+          )
+              .white
+              .size(
+                context.screenWidth,
+                context.percentHeight * 18,
+              )
+              .rounded
+              .shadowXl
+              .make()
+              .p16(),
+          HStack(
+            [
+              TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.refresh, color: Colors.white),
+                label: "Refresh".text.white.make(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blue),
+                  shape: MaterialStateProperty.all(
+                    const RoundedRectangleBorder(),
+                  ),
+                ),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.call_made_outlined, color: Colors.white),
+                label: "Deposit".text.white.make(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.green),
+                  shape: MaterialStateProperty.all(
+                    const RoundedRectangleBorder(),
+                  ),
+                ),
+              ),
+              TextButton.icon(
+                onPressed: () {},
+                icon: Icon(Icons.call_received_outlined, color: Colors.white),
+                label: "Withdraw".text.white.make(),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                  shape: MaterialStateProperty.all(
+                    const RoundedRectangleBorder(),
+                  ),
+                ),
+              ),
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+            axisSize: MainAxisSize.max,
+          )
+        ])
+      ]),
+    );
+  }
+}
--- wallet_blockchain/lib/main.dart
+++ wallet_blockchain/lib/main.dart
@@ -1,22 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'Demo Demo',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
     );
   }
 }
